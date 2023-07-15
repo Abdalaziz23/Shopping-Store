885fc0a470cd878705a093e08dd11350b06db018
Abdalaziz Mohmmed <120694450+Abdalaziz23@users.noreply.github.com>
2023-07-15 17:54:43 +0300

    eds

--- lib/layout/shop_app/shop_layout.dart
+++ lib/layout/shop_app/shop_layout.dart
@@ -20,7 +20,7 @@ class ShopLayout extends StatelessWidget {
         return Scaffold(
           appBar: AppBar(
             title: const Text(
-              'Football',
+              'Shopping Store',
             ),
             actions: [
               IconButton(
